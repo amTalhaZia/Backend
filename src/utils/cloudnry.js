a78db4a49ebeab75f8fff664fe9e6ff0ae5d069d
amTalhaZia <amtalha.zia@gmail.com>
2024-09-22 01:41:48 -0700

    commit

--- src/utils/cloudnry.js
+++ src/utils/cloudnry.js
@@ -18,12 +18,14 @@ const uploadCloudinary = async (localFilePath) => {
           resource_type: 'auto'
          })
   
-         console.log("filw  is uploaded :",response.url );
+         console.log("file  is uploaded :",response.url );
          return response
        } catch (error) {
            fs.unlinkSync(localFilePath)
+           return null
        }
 }
+
 // const uploadResult = await cloudinary.uploader
 // .upload(
 //     'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
@@ -33,3 +35,6 @@ const uploadCloudinary = async (localFilePath) => {
 // .catch((error) => {
 //     console.log(error);
 // });
+
+
+export {uploadCloudinary}
